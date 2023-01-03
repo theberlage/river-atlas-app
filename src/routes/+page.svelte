935c601ffe3785c01b515eb313af11369fec5419
Jules Schoonman <jules.schoonman@gmail.com>
2023-01-03 12:26:06 +0100

    plugin test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,20 +8,49 @@
 	import 'ol/ol.css'
 	import { formatNumber } from '$lib/intl.js'
 
-  import { WarpedMapLayer } from '@allmaps/openlayers'
+  import { WarpedMapSource, WarpedMapLayer } from '@allmaps/openlayers'
 
   import Jules from '$lib/components/Jules.svelte'
 
 	let map: Map
 	let view: View
+
+  let warpedMapSource: WarpedMapSource
   let warpedMapLayer: WarpedMapLayer
 
 	let rotation: number = 0
   let data = {}
 
-	onMount(() => {
+  async function fetchJson(url: any) {
+  return fetch(url).then((response) => response.json());
+}
 
-    warpedMapLayer = new WarpedMapLayer()
+  let annotationUrls = [
+  // West-Roxbury
+  "https://raw.githubusercontent.com/allmaps/webgl2-preview/main/static/west-roxbury.json",
+  // Roxbury
+  "https://annotations.allmaps.org/images/25b19ade19654e66",
+  // Provincetown
+  "https://annotations.allmaps.org/?url=https://collections.leventhalmap.org/search/commonwealth:0r96fq56q/manifest",
+  // Cambridge
+  "https://annotations.allmaps.org/?url=https://collections.leventhalmap.org/search/commonwealth:wd376290m/manifest"
+  ]
+
+
+	onMount(async () => {
+    let annotations = await Promise.all(annotationUrls.map(fetchJson))
+
+    warpedMapSource = new WarpedMapSource()
+
+    warpedMapLayer = new WarpedMapLayer({
+      source: warpedMapSource
+    })
+
+    for (let annotation of annotations) {
+      await warpedMapSource.addGeorefAnnotation(annotation);
+    }
+
+
 
     console.log(warpedMapLayer)
 		view = new View({
@@ -34,7 +63,8 @@
 			layers: [
 				new TileLayer({
 					source: new OSM()
-				})
+				}),
+        warpedMapLayer
 			],
 			target: 'map'
 		})
